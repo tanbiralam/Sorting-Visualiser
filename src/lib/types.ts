8be9f94dc2522f6ed9b1f0c9909c629f097ec2c0
tanbiralam <itzmetanbir@gmail.com>
2024-09-10 21:11:24 +0530

    Visualizer Context Done

---
+++ src/lib/types.ts
@@ -0,0 +1,8 @@
+export type SortingAlgorithmType =
+  | "bubble"
+  | "insertion"
+  | "selection"
+  | "merge"
+  | "quick";
+
+export type AnimateSpeedType = "slow" | "medium" | "fast" | "lighning";
